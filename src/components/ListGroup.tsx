9bcdf3c95f2118571f669d6c9c7b792f6c7394e4
parisa <parisa.naeim@gmail.com>
2023-08-02 22:57:48 +1000

    list group component

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,4 +1,17 @@
 function ListGroup() {
-  return <h1>List Group</h1>;
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  items = [];
+  return (
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
+
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
 export default ListGroup;
